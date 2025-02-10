c769ce669d017ab19081d037dbf4c202887e647f
gabrielfmcoelho <gabrielcoelho09gc@gmail.com>
2025-02-10 19:02:49 -0300

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css'
 import { ColorDivider } from '@/components/ColorDivider'
 
 export const metadata: Metadata = {
-  title: 'Soluções Digitais SEAD',
-  description: 'Soluções digitais em desenvolvimento pela SEAD',
+  title: 'Soluções Digitais',
+  description: 'Soluções digitais em desenvolvimento - SUR Systems'
 }
 
 export default function RootLayout({
